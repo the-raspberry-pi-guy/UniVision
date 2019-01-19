a4a3a23d1563d5fd963c2f1297436e296566eb27
the-raspberry-pi-guy <theraspberrypiguy@gmail.com>
2019-01-19 16:20:24 +0000

    Remove close from training group

--- main.py
+++ main.py
@@ -67,7 +67,6 @@ def trainGroup(targetGroup):
         response = conn.getresponse()
         data = response.read()
         print(data)
-        conn.close()
     except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
