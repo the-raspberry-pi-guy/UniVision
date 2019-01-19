6a8b03b1a172d6fca09cdd8c86d6052ba5059cd9
Neil Weidinger <neilweidingern@gmail.com>
2019-01-19 15:59:03 +0000

    added main file

---
+++ main.py
@@ -0,0 +1,29 @@
+import http.client, urllib.request, urllib.parse, urllib.error, base64, json
+
+headers = {
+    # Request headers
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': '700bad17d5d6443bad2fd69b0da27cdc',
+}
+
+conn = http.client.HTTPSConnection('northeurope.api.cognitive.microsoft.com')
+
+def createGroup(groupID, groupName):
+
+    params = urllib.parse.urlencode({})
+
+    body = {
+            "name" : '{}'.format(groupName),
+            }
+
+    try:
+        conn.request("PUT", "/face/v1.0/persongroups/" + groupID + "?%s" % params, json.dumps(body), headers)
+        response = conn.getresponse()
+        data = response.read()
+        print(data)
+        conn.close()
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+if __name__ == "__main__":
+    createGroup("testgroup", "hello group")
